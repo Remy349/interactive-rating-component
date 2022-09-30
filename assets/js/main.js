01e1985a8937bdfaaebf4940d1a78f098b0f5ab3
Remy349 <santoraga15@gmail.com>
2022-09-30 12:53:14 -0600

    Validating submit button

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const btnsRating = document.querySelectorAll('.rating__card-rate_btn')
   const number = document.getElementById('number')
 
-  btnSubmit.addEventListener('click', () => {
-    const ratingCard = document.getElementById('ratingCard')
-    const ratingAnswer = document.getElementById('ratingAnswer')
-
-    ratingCard.style.display = 'none'
-    ratingAnswer.style.display = 'block'
-  })
+  let value = '0'
 
   btnsRating.forEach((btn) => {
     btn.addEventListener('click', () => {
       btn.classList.toggle('change-rating-btn')
 
-      number.innerHTML = btn.innerHTML
+      value = btn.innerHTML
+
+      number.innerHTML = value
+
     })
+  })
+
+  btnSubmit.addEventListener('click', () => {
+    const ratingCard = document.getElementById('ratingCard')
+    const ratingAnswer = document.getElementById('ratingAnswer')
+
+    if (value == '0') {
+      alert('Please, choose one value!')
+    } else {
+      ratingCard.style.display = 'none'
+      ratingAnswer.style.display = 'block'
+    }
   })
 })
 
