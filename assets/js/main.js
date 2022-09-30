cc069519a22d25a7bb14307909f002c269131ca9
Remy349 <santoraga15@gmail.com>
2022-09-29 22:57:25 -0600

    Project completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Working...')
+  const btnSubmit = document.getElementById('btnSubmit')
+  const btnsRating = document.querySelectorAll('.rating__card-rate_btn')
+  const number = document.getElementById('number')
+
+  btnSubmit.addEventListener('click', () => {
+    const ratingCard = document.getElementById('ratingCard')
+    const ratingAnswer = document.getElementById('ratingAnswer')
+
+    ratingCard.style.display = 'none'
+    ratingAnswer.style.display = 'block'
+  })
+
+  btnsRating.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('change-rating-btn')
+
+      number.innerHTML = btn.innerHTML
+    })
+  })
 })
 
